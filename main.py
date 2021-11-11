a2222773cf389ca4bd43cb21bc2e7d91543d5748
Daniel Piper <piperdaniel1@gmail.com>
2021-11-10 23:09:57 -0800

    Added js script to find passage to copy

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ import time
 prompt = input("enter type prompt here: ")
 target_wpm = 100
 
-print("press s to start...")
-while(keyboard.is_pressed('s') == False):
+while(keyboard.is_pressed('enter') == False):
     pass
 
 for char in prompt:
     keyboard.write(char)
-    time.sleep(1/target_wpm)
+    time.sleep(0.08)
+
+'''
+var span_arr = document.getElementsByTagName("span")
+var current_longest = 0
+var current_index = 0
+
+for(var i=0; i<span_arr.length; i++) {
+    if(span_arr[i].className === "") {
+        if(span_arr[i].textContent.length > current_longest) {
+            current_index = i
+            current_longest = span_arr[i].textContent.length
+        }
+    }
+}
+
+if(span_arr[current_index-2].textContent.length === 1) {
+    console.log(span_arr[current_index-2].textContent + span_arr[current_index-1].textContent + span_arr[current_index].textContent)
+} else {
+    console.log(span_arr[current_index-1].textContent + span_arr[current_index].textContent)
+}
+'''
